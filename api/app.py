adf54294e9e3b1a3522dc8fedaa4457395c60915
arthur <arthur.silva@irede.org.br>
2023-11-16 15:29:06 -0300

    feat upload file

--- api/app.py
+++ api/app.py
@@ -1,20 +1,27 @@
-from flask import Flask
-from flask import json
+from flask import Flask, json, request, Response
 from werkzeug.exceptions import *
 
 
 app = Flask(__name__)
 
 @app.errorhandler(HTTPException)
-def handle_exception(e):
+def handle_exception(e : HTTPException):
     print(e)
     return json.dumps({
         "code": e.code,
-        "description": "teste",
+        "description": e.name,
         })
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(f'../storage/{f.filename}.txt')
+        return Response(status=201)
+    raise MethodNotAllowed()
 
 
