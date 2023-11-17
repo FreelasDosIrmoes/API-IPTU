95c26dabda6ab278fb8c9eb2c328e75141a97909
arthur <arthur.silva@irede.org.br>
2023-11-17 15:48:19 -0300

    fix route

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ from api.service import *
 
 
 app = Flask(__name__)
-
+PATH_DEFAULT = "/api/iptu"
 
 @app.errorhandler(HTTPException)
 def handle_exception(e: HTTPException):
@@ -17,14 +17,14 @@ def handle_exception(e: HTTPException):
     )
 
 
-@app.route("/<int:iptu_code>")
+@app.route(f"{PATH_DEFAULT}/<int:iptu_code>")
 def hello_world(iptu_code : int):
     return make_response({
         "iptu_code": iptu_code
     })
 
 
-@app.route("/upload", methods=['POST'])
+@app.route(f"{PATH_DEFAULT}/upload", methods=['POST'])
 def upload():
     if request.method == "POST":
         f = handling_get_file('file', request)
