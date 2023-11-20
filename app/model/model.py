b560562a558648495beb54c4116a53940709e56d
arthur <arthur.silva@irede.org.br>
2023-11-20 15:13:13 -0300

    mend

--- app/model/model.py
+++ app/model/model.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy import ForeignKey
+
 db = SQLAlchemy()
+
 
 class Iptu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,12 +14,14 @@ class Iptu(db.Model):
     cobranca = db.relationship("Cobranca", backref="iptu")
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
+
 class Dono(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100))
     numero = db.Column(db.String(20), unique=True, nullable=False)
     iptu_id = db.Column(db.Integer, ForeignKey('iptu.id'))
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
 
 class Cobranca(db.Model):
     id = db.Column(db.Integer, primary_key=True)
