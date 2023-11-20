85a923bbd03a8fb26baf05d6d6879071bddd2ac2
arthur <arthur.queiroz08@aluno.ifce.edu.br>
2023-11-19 22:49:23 -0300

    fix requirements and add in .env.template

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import *
 from app.model.model import db, Iptu
 from app.service import handling_get_file
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_NAME = os.getenv('DB_NAME')
+DB_PORT = os.getenv('DB_PORT')
+DB_HOST = os.getenv('DB_HOST')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/iptu-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:postgres@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
