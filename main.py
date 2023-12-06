1f2c1bbc3d7d3365c0d3b64dbf1d5824a625db26
JorgeVitor30 <jorgevitorlima8@gmail.com>
2023-12-05 21:34:22 -0300

    .

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# from app import app_flask
+from app import app_flask
 
-# if __name__ == "__main__":
-#     app_flask.run(debug=True)
-
-from rpa.rpa import Automation
-
-robot = Automation()
-robot.process_flux_current_year('48517852','JORGE')
+if __name__ == "__main__":
+    app_flask.run(debug=True)
