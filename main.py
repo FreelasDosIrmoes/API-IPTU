add024aa43db450f297094db5a6af82111d1f9eb
JorgeVitor30 <jorgevitorlima8@gmail.com>
2024-01-08 21:51:31 -0300

    .

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from app import app_flask
 from app.service import schedule_process
 from rpa.rpa import Automation
 
-# if __name__ == "__main__":
-#     threading.Thread(target=schedule_process, args=(app_flask,)).start()
-#     app_flask.run(debug=False, port=5001, host="0.0.0.0")
+if __name__ == "__main__":
+    threading.Thread(target=schedule_process, args=(app_flask,)).start()
+    app_flask.run(debug=False, port=5001, host="0.0.0.0")
 
-from rpa.rpa import Automation
-
-robot = Automation()
-robot.process_flux_current_year('06025595', '')
